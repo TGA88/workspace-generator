f94cb689a499167a1a6fff99ab249a9f99149323
best <supawut.thomas@gmail.com>
2024-12-15 22:30:37 +0700

    feat: add custom spacing base4 for tailwind

--- feedos-example-system/workspaces/node-app/libs/feature-exm/lib/components/user-management-layout/user-management-layout.tsx
+++ feedos-example-system/workspaces/node-app/libs/feature-exm/lib/components/user-management-layout/user-management-layout.tsx
@@ -12,7 +12,7 @@ export const UserManagementLayout: React.FC<UserManagementLayoutProps> = ({
 }) => {
   return (
     <Stack
-      className="fos-space-y-3 fos-px-3 fos-py-2 fos-bg-red-200"
+      className="fos-space-y-b4-6 fos-px-b4-20 fos-py-b4-2 fos-bg-red-200"
       sx={{ 
         '& > *': { width: '100%' } // ให้ children ใช้พื้นที่เต็มความกว้าง
       }}
