33e6eb8e996f06cbecb4ac05f269668722687208
best <supawut.thomas@gmail.com>
2025-01-05 18:54:41 +0700

    feat(gen): add script new-web.sh

---
+++ script-generator/new-web.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+# WORKSPACE_DIR='feedos-example-system'
+# PROJECT_NAME='feature-example'
+WORKSPACE_DIR=$1
+PROJECT_NAME=$2
+GENERATOR_DIR=$3
+SYSTEM_DIR='node-app'
+
+
+
+if [ -z "$GENERATOR_DIR" ]; then
+    echo "ตัวแปร GENERATOR_DIR ไม่มีค่า หรือมีค่าว่าง"
+    if [ -d "workspace-generator" ]; then
+        echo "มีโฟลเดอร์ workspace-generator"
+        GENERATOR_DIR="workspace-generator"
+        echo "กำหนด GENERATOR_DIR='workspace-generator' "
+    else
+        echo "ไม่มีโฟลเดอร์ workspace-generator"
+        GENERATOR_DIR="."
+        echo "กำหนด GENERATOR_DIR='.' "
+    fi
+else
+  echo "ตัวแปร GENERATOR_DIR มีค่า: $GENERATOR_DIR"
+fi
+
+# Check if PROJECT_NAME is provided
+if [ -z "$PROJECT_NAME" ]; then
+   echo "Error: PROJECT_NAME is not set"
+   exit 1
+fi
+
+
+CUR_PATH=$(pwd)
+
+# สร้าง folder project
+# mkdir -p <workspace_dir>/workspaces/<system_name>
+echo 'mkdir -p $WORKSPACE_DIR/workspaces/$SYSTEM_DIR/apps/$PROJECT_NAME/nextjs'
+mkdir -p $WORKSPACE_DIR/workspaces/$SYSTEM_DIR/apps/$PROJECT_NAME/nextjs
+
+
+
+
+cp -r $GENERATOR_DIR/script-generator/template/project/web/nextjs/* $WORKSPACE_DIR/workspaces/$SYSTEM_DIR/apps/$PROJECT_NAME/nextjs/
+cp -r $GENERATOR_DIR/script-generator/template/project/web/nextjs/.env.example $WORKSPACE_DIR/workspaces/$SYSTEM_DIR/apps/$PROJECT_NAME/nextjs/.env
+
+cd $WORKSPACE_DIR/workspaces/$SYSTEM_DIR/apps/$PROJECT_NAME/nextjs
+# Search and replace in all files under web directory
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    find ./ -type f -not -path "*/\.*" -exec file {} \; | 
+    grep -i -E '(text| JSON data)' | 
+    cut -d: -f1 | 
+    xargs sed -i '' -e "s/demo-exm-web/$PROJECT_NAME/g" -e "s/feedos-example-system/$WORKSPACE_DIR/g"
+    # xargs sed -i '' "s/@feature-exm/@$PROJECT_NAME/g"
+
+else
+
+    find ./ -type f -not -path "*/\.*" -exec file {} \; | 
+    grep -i -E '(text| JSON data)' | 
+    cut -d: -f1 | 
+     xargs sed -i -e "s/demo-exm-web/$PROJECT_NAME/g" -e "s/feedos-example-system/$WORKSPACE_DIR/g"
+    # xargs sed -i '' "s/@feature-exm/@$PROJECT_NAME/g"
+
+fi
+
+echo "Replaced 'demo-exm-web' with '$PROJECT_NAME' in all files under $WORKSPACE_DIR/workspaces/$SYSTEM_DIR/apps/$PROJECT_NAME/nextjs/"
+
+
+npm pkg set name=$PROJECT_NAME-nextjs
+npm pkg set scripts.fix:lcov="bash ../../../tools/fix_lcov_paths.sh ../../../coverage/apps/"$PROJECT_NAME/nextjs
+
+# add inh-lib/common , inh-lib/ddd
+# pnpm add -w @inh-lib/common @inh-lib/ddd
+
+pnpm install
+pnpm update -i
+
+
+
+
+
--- script-generator/template/project/web/nextjs/app/(public)/layout.tsx
+++ script-generator/template/project/web/nextjs/app/(public)/layout.tsx
@@ -1,10 +1,27 @@
-import { ReactNode } from 'react';
-import { PublicLayout } from '../components/publicLayout';
+import React from 'react';
+import '../globals.css';
+import { NextIntlClientProvider } from 'next-intl';
+import { getMessages } from 'next-intl/server';
+import { cookies } from 'next/headers';
 
-interface Props {
-  children: ReactNode;
-}
 
-export default async function SsoLayout({ children }: Props) {
-  return <PublicLayout>{children}</PublicLayout>;
+export default async function LocaleLayout({
+  children,
+  params: { locale },
+}: Readonly<{
+  children: React.ReactNode;
+  params: { locale: string };
+}>) {
+  const messages = await getMessages();
+  const cookieStore = cookies();
+  const lang = cookieStore.get('NEXT_LOCALE')?.value;
+  return (
+    <html lang={locale ?? lang}>
+      <body>
+        <NextIntlClientProvider messages={messages}>
+          {children}
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
 }
