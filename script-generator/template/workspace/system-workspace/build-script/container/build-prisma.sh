455e3770c25978bb38368272b5bc4c9eca5dcfa3
best <supawut.thomas@gmail.com>
2025-02-19 11:12:48 +0700

    fix(template): schema.prisma and build-script contianer to copy  file linux-musl-arm64-openssl-1.1.x todeploy in container

--- script-generator/template/workspace/system-workspace/build-script/container/build-prisma.sh
+++ script-generator/template/workspace/system-workspace/build-script/container/build-prisma.sh
@@ -1,6 +1,6 @@
 # Copy dependency for hosting (linux-alpine) in Docker
 echo "copy libquery_engine-linux-musl-openssl-x.x.x.so.node to release folder"
-for file in $(find node_modules/prisma/ | grep "libquery_engine-linux-musl-" );
+for file in $(find node_modules/prisma/ | grep "libquery_engine-linux-musl" );
 do
   echo "cp $file root"
   cp $file .
